30a67776cd83bcf477d516b15e3cbc5a5406435c
kellyxc28 <kellyxiongc@gmail.com>
2024-06-15 09:24:16 -0400

    Completed Coding Portion

--- src/from-scratch.js
+++ src/from-scratch.js
@@ -1,2 +1,29 @@
-export const fetchHandler = () => {
+export const fetchHandler = async (url, options = {}) => {
+  try {
+    // const tuple = [data, { message: 'error!' }];
+    const response = await fetch(url, options);
+
+    if (!response.ok)
+      throw new Error(
+        `Fetch failed with status - ${response.status}, ${response.statusText}`
+      );
+
+    // get the headers from the response.headers
+    const isJson = (response.headers.get('content-type') || '').includes(
+      'application/json'
+    );
+
+    if (isJson) {
+      const jsonData = await response.json();
+      return [jsonData, null];
+    } else {
+      const textData = await response.text();
+      return [textData, null];
+    }
+
+    // return [{}, null];
+  } catch (error) {
+    console.warn(error);
+    return [null, error];
+  }
 };
